78429b9ef87bda1f4ba1ca5042caacaf6356f14a
joblessnovice <102379490+joblessnovice@users.noreply.github.com>
2022-12-09 15:13:38 -0500

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+document.childNodes[1][0][2].write("   <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="course.html">Course</a></li>
+                <li><a href="membership.html">Membership</a></li>
+                <li><a href="rates.html">Rates</a></li>
+                <li><a href="instruction.html">Instruction</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="etiquette.html">Etiquette</a></li>
+                <li><a href="rules.html">Rules</a></li>
+            </ul>
+        </nav>")
