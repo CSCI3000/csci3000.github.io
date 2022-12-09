0f7efde4a8757d3c6d2747a597e5579a1698aa6d
joblessnovice <102379490+joblessnovice@users.noreply.github.com>
2022-12-09 15:17:28 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
-document.childNodes[1][0][2].write("   <nav>
+document.childNodes[1][0].write(  <header>
+        <img src="assets/logo.png" class="logo" alt="logo" width="200">
+        <input type="checkbox" id="nav-toggle" class="nav-toggle">
+        <nav>
             <ul>
-                <li><a href="../index.html">Home</a></li>
-                <li><a href="course.html">Course</a></li>
-                <li><a href="membership.html">Membership</a></li>
-                <li><a href="rates.html">Rates</a></li>
-                <li><a href="instruction.html">Instruction</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="etiquette.html">Etiquette</a></li>
-                <li><a href="rules.html">Rules</a></li>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="html/course.html">Course</a></li>
+                <li><a href="html/membership.html">Membership</a></li>
+                <li><a href="html/rates.html">Rates</a></li>
+                <li><a href="html/instruction.html">Instruction</a></li>
+                <li><a href="html/contact.html">Contact</a></li>
+                <li><a href="html/etiquette.html">Etiquette</a></li>
+                <li><a href="html/rules.html">Rules</a></li>
             </ul>
-        </nav>")
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label">
+            <span></span>
+        </label>
+    </header>)
