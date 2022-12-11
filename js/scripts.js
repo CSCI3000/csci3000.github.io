881de8b9362520f767486134cdaefff0a5c86c09
joblessnovice <102379490+joblessnovice@users.noreply.github.com>
2022-12-10 19:55:28 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let timerId = setTimeout(showHoliday, 3000);
-let endTimer = clearTimeout(timerId,10000);
+let endTimer = clearTimeout(timerId);
 function showHoliday() {
    let holiday  = document.getElementById("holiday");
    holiday.style.display = "block";
