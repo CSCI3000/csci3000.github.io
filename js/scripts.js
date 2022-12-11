7bf4edc3d5343433e33da006ea2bbd50227d8d48
joblessnovice <102379490+joblessnovice@users.noreply.github.com>
2022-12-10 20:58:42 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var holidayMessage = document.getElementById("holiday");
 let msg = setTimeout(showMsg,3000);
-holiday.addEventListener('click',clearinterval(msg));
+holidayMessage.addEventListener('click',clearinterval(msg));
 function showMsg(){
    holidayMessage.style.visibility: visible;
 }
